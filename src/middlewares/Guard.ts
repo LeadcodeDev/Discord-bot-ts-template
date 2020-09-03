c7bc134baf8dffd03f5ab6565704b7cb20e1ea60
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-03 13:26:50 +0200

    :bug: fix(guard): allow empty roles

--- src/middlewares/Guard.ts
+++ src/middlewares/Guard.ts
@@ -17,12 +17,17 @@ class Guard extends Middleware {
 				.forEach(async (command) => {
 					const { roles, name } = command
 					await message.delete()
-					if (this.hasRoles(roles, sender)) {
+					if (roles.length != 0) {
+						if (this.hasRoles(roles, sender)) {
+							await command.run(message, args.slice(1))
+							await Logger.emit('logger', Types.INFO, `${author.tag} execute command (${name})`, false)
+						} else {
+							await sender.lastMessage!.reply("Vous n'avez pas l'autorisation d'excuter cette commande.")
+							await Logger.emit('logger', Types.ERROR, `${author.tag} not allowed to execute command (${command.name})`)
+						}
+					} else {
 						await command.run(message, args.slice(1))
 						await Logger.emit('logger', Types.INFO, `${author.tag} execute command (${name})`, false)
-					} else {
-						await sender.lastMessage!.reply("Vous n'avez pas l'autorisation d'excuter cette commande.")
-						await Logger.emit('logger', Types.ERROR, `${author.tag} not allowed to execute command (${command.name})`)
 					}
 				})
 		})
