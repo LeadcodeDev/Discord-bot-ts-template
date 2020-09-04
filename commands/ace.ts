73df8f3f5acb066351c85a13cb12493a9d3cd05b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-04 16:46:11 +0200

    :sparkles: feature(command generator): create command to create events into bot

--- commands/ace.ts
+++ commands/ace.ts
@@ -5,21 +5,30 @@ const args = props.split(',')
 if (commandName == 'make:command') {
 	if (args[0] != 'undefined') {
 		const templateDir = process.cwd() + '/core/template/command'
-		fs.readFile(templateDir, 'utf8', (err, data) => {
-			const fileNameUpperCase = args[0].charAt(0).toUpperCase() + args[0].slice(1)
-			const newFile = data.replace(/Example/g, fileNameUpperCase)
-			const commandDir = process.cwd() + '/src/commands'
-
-			fs.access(`${commandDir}/${fileNameUpperCase}.ts`, fs.constants.F_OK, async (err) => {
-				// if (err == null) Logger.emit('logger', Types.FATAL, `File already exist in ${commandDir}`)
-				if (err?.code == 'ENOENT') {
-					fs.writeFile(`${commandDir}/${fileNameUpperCase}.ts`, newFile, (error) => {
-						if (!err) console.log(`File ${fileNameUpperCase} was create`)
-					})
-				} else {
-					return console.log(`File already exist in "${commandDir.replace(/\\/g, '/')}"`)
-				}
-			})
-		})
+		const commandDir = process.cwd() + '/src/commands'
+		makeFile(templateDir, commandDir)
 	}
+} else if (commandName == 'make:event') {
+	const templateDir = process.cwd() + '/core/template/event'
+	const eventDir = process.cwd() + '/src/events'
+	if (args[0] != 'undefined') {
+		makeFile(templateDir, eventDir, args[0])
+	}
+}
+
+function makeFile(templateDir: string, folderDir: string, eventName: string = '') {
+	fs.readFile(templateDir, 'utf8', (err, data) => {
+		const fileNameUpperCase = args[0].charAt(0).toUpperCase() + args[0].slice(1)
+		let newFile = data.replace(/Example/g, fileNameUpperCase).replace(/event/g, eventName.charAt(0).toLowerCase() + eventName.slice(1))
+
+		fs.access(`${folderDir}/${fileNameUpperCase}.ts`, fs.constants.F_OK, async (err) => {
+			if (err?.code == 'ENOENT') {
+				fs.writeFile(`${folderDir}/${fileNameUpperCase}.ts`, newFile, (error) => {
+					if (!err) console.log(`File ${fileNameUpperCase} was create`)
+				})
+			} else {
+				return console.log(`File already exist in "${folderDir.replace(/\\/g, '/')}"`)
+			}
+		})
+	})
 }
