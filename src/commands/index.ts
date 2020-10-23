b821ea17bd81f1a5e9655d0f0bfc73f31956c9e9
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-10-24 01:25:09 +0200

    :construction: Remove older file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,4 @@
-import Test from './Test'
-import Band from './Band'
-import Ban from './Ban'
 import Help from './Help'
 import Example from './Example'
 
-export { Example, Help, Ban, Band, Test }
+export { Example, Help }
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,4 @@
-import GuilMemberRemove from './GuilMemberRemove'
-import GuildMemberAdd from './GuildMemberAdd'
 import Ready from './Ready'
 import Message from './Message'
 
-export { Ready, Message, GuildMemberAdd, GuilMemberRemove }
+export { Ready, Message }
