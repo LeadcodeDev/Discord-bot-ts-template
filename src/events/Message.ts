dc9487d00bcb4386ab17ec29642dabb69166da65
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-11-02 18:28:49 +0100

    :construction: Work in progress

--- src/events/Message.ts
+++ src/events/Message.ts
@@ -1,8 +1,8 @@
-import { Message } from 'discord.js'
 import { EventInterface } from '../interfaces'
 import { Guard } from '../../core/Middlewares'
 import { EventList } from '../types'
 import { Event } from './../interfaces/decorators'
+import { Message } from 'discord.js'
 
 @Event({ type: EventList.MESSAGE })
 class ReadyEvent implements EventInterface {
