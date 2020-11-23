ba0fa944bba1b3a0665caadfc7e5dd468a4b8d53
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-11-23 22:18:12 +0100

    :construction: Implement self-instanciation

--- src/middlewares/Example.ts
+++ src/middlewares/Example.ts
@@ -3,10 +3,8 @@ import { Middleware } from 'Core/decorators'
 import { MiddlewareInterface } from 'Core/interfaces'
 
 @Middleware({ lifecycle: Hooks.EXAMPLE })
-class Example extends MiddlewareInterface {
+export default class Example extends MiddlewareInterface {
 	public async run(): Promise<void> {
 		// Your code here
 	}
 }
-
-export default new Example()
