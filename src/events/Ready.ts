a54b81d07409225293c4a65a4a3a315f4249b9f9
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-12-22 19:53:25 +0100

    :construction: Add cross-env

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,7 +1,7 @@
 import { Event, Events } from '@discord-ts-app/core/build/Decorators/Class'
 import { EventInterface } from '@discord-ts-app/core/build/Interfaces'
 import { Logger, State } from '@discord-ts-app/core/build/Utils/Logger'
-import Env from '@discord-ts-app/core/build/Utils/Env'
+import Env from '@discord-ts-app/core/build/utils/Env'
 
 @Event({ type: Events.READY })
 export default class ReadyEvent implements EventInterface {
